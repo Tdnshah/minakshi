2b89ed5233326cdc7f3bfaad422c2ce8ec4ab27b
Tejas <t.shah@vardot.com>
2026-03-01 17:32:48 +0530

    Enable Payload cms users with api key.

--- minakshi-web/cms/src/collections/Users.ts
+++ minakshi-web/cms/src/collections/Users.ts
@@ -5,7 +5,9 @@ export const Users: CollectionConfig = {
   admin: {
     useAsTitle: 'email',
   },
-  auth: true,
+  auth: {
+    useAPIKey: true, // This enables API key generation in the Admin UI
+  },
   fields: [
     // Email added by default
     // Add more fields as needed
